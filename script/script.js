8396fe4ce1cb023f892588d1427ef25ed2a3aa6f
AlesLearningCode <146716296+AlesLearningCode@users.noreply.github.com>
2023-11-05 10:11:31 +0100

    Change logic of the game

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 let Gameboard = {
     gameboard: [],
-    newArray:[],
+    counts:{},
     init: function(){  
         this.render()
         this.playerMark()
@@ -16,39 +16,67 @@ let Gameboard = {
     },
     playerMark: function(){
         const board = document.querySelector(`.game`)
+        const endScore = document.querySelector(`.endScore`)
         const squares = document.querySelectorAll(`.square`)
         squares.forEach(square => {
             square.addEventListener(`click`, () =>{
-                // input of squares onto gameboard
-                if(this.arrayLastIndex(1) == `O` && this.gameboard.length < 9 && 
+                // searches for how many occurences of each sign is in array
+                const occurrences = this.gameboard.reduce(function (acc, curr) {
+                    return acc[curr] ? ++acc[curr] : acc[curr] = 1, acc
+                  }, {});
+                // input of player marks onto gameboard
+                if(this.gameboard.length === 0){
+                endScore.textContent = " "
+                this.gameboard[square.getAttribute(`number`)] = 1
+                square.innerHTML = `X`
+                }else if((occurrences[1] > occurrences[2] || occurrences[2] == undefined) && 
                 square.textContent == ""){
-                this.gameboard.push(`X`)
-                this.newArray[square.getAttribute(`number`)] = "X"
-                square.innerHTML = this.gameboard[this.gameboard.length - 1]
-                }else if(this.arrayLastIndex(1) == `X` && this.gameboard.length < 9 && 
+                square.innerHTML = `O`
+                this.gameboard[square.getAttribute(`number`)] = 2
+                }else if(occurrences[2] === occurrences[1] && 
                 square.textContent == ""){
-                this.gameboard.push(`O`)
-                square.innerHTML = this.gameboard[this.gameboard.length - 1]
-                this.newArray[square.getAttribute(`number`)] = "O"
-                }else if(this.gameboard.length === 9){
+                square.innerHTML = `X`
+                this.gameboard[square.getAttribute(`number`)] = 1
+                }else if(occurrences[1] + occurrences[2] == 9){
                 this.gameboard.splice(0, 9)
-                this.newArray.splice(0,9)
                 board.innerHTML = ""
                 this.init()
-                }else if(this.gameboard.length === 0){
-                this.gameboard.push(`X`)
-                square.innerHTML = `X`
-                this.newArray[square.getAttribute(`number`)] = "X"
                 }
-                // Checks for logic of the game and decides winner
-                if(){
-                    console.log("You won")
-                    board.innerHTML = ""
-                    this.newArray.splice(0,9)
-                    this.gameboard.splice(0, 9)
-                    this.init()
+                //Checks for logic of the game and decides winner 
+                if((this.gameboard[0] == 1 && this.gameboard[1] == 1 && this.gameboard[2] == 1) || 
+                (this.gameboard[0] == 2 && this.gameboard[1] == 2 && this.gameboard[2] == 2) ||
+                (this.gameboard[3] == 1 && this.gameboard[4] == 1 && this.gameboard[5] == 1) || 
+                (this.gameboard[3] == 2 && this.gameboard[4] == 2 && this.gameboard[5] == 2) ||
+                (this.gameboard[6] == 1 && this.gameboard[7] == 1 && this.gameboard[8] == 1) || 
+                (this.gameboard[6] == 2 && this.gameboard[7] == 2 && this.gameboard[8] == 2)){
+                endScore.textContent = `You win`
+                this.gameboard.splice(0, 9)
+                board.innerHTML = ""
+                this.init()
+                }else if((this.gameboard[0] == 1 && this.gameboard[3] == 1 && this.gameboard[6] == 1) || 
+                (this.gameboard[0] == 2 && this.gameboard[3] == 2 && this.gameboard[6] == 2) ||
+                (this.gameboard[1] == 1 && this.gameboard[4] == 1 && this.gameboard[7] == 1) || 
+                (this.gameboard[1] == 2 && this.gameboard[4] == 2 && this.gameboard[7] == 2) ||
+                (this.gameboard[2] == 1 && this.gameboard[5] == 1 && this.gameboard[8] == 1) || 
+                (this.gameboard[2] == 2 && this.gameboard[5] == 2 && this.gameboard[8] == 2)){
+                endScore.textContent = `You win`
+                this.gameboard.splice(0, 9)
+                board.innerHTML = ""
+                this.init()
+                }else if((this.gameboard[0] == 1 && this.gameboard[4] == 1 && this.gameboard[8] == 1) || 
+                (this.gameboard[0] == 2 && this.gameboard[4] == 2 && this.gameboard[8] == 2) ||
+                (this.gameboard[2] == 1 && this.gameboard[4] == 1 && this.gameboard[6] == 1) || 
+                (this.gameboard[2] == 2 && this.gameboard[4] == 2 && this.gameboard[6] == 2)){
+                endScore.textContent = `You win`
+                this.gameboard.splice(0, 9)
+                board.innerHTML = ""
+                this.init()
+                }else if(occurrences[1] + occurrences[2] == 8){
+                endScore.textContent = `It's a draw`
+                this.gameboard.splice(0, 9)
+                board.innerHTML = ""
+                this.init()
                 }
-                console.log(this.newArray)
             })
         });
     },
